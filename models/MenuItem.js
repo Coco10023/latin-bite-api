972d7f74bb4e8b47e0cfe5e0f714f59b42251376
Coco10023 <mauricio2003@live.se>
2026-05-17 18:17:29 +0200

    Skapar en modell

---
+++ models/MenuItem.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("MenuItem", menuItemSchema);
